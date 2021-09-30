8b35c0e015c7f65a7fa0b62b5fb8a8c0a7fbdd5d
c_h_o <c_h_o@DESKTOP-RPURV9O>
2021-09-30 20:16:49 +0200

    Cambio

--- Compartido/src/compartido1/PruebaCompartida1.java
+++ Compartido/src/compartido1/PruebaCompartida1.java
@@ -4,7 +4,7 @@ public class PruebaCompartida1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!");
-		System.out.println("Prueba de que cambia");
+		System.out.println("He cambiado");
 	}
 
 }
